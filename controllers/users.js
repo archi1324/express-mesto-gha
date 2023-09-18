4675e1460ad90a823ba80f4684a12a3b72807e1d
Артур Саяхов <artursaahov@MacBook-Air-Artur.local>
2023-09-18 21:27:08 +0500

    third try fix user bags

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,13 @@ const User = require('../models/user');
 module.exports.getUser = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(500).send({ message: 'Ошибка 500' }));
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
-    .catch((err) => res.send({ message: err}))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Невозможно создать пользователя' });
--- models/user.js
+++ models/user.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поле Name не заполнено'],
-    minlength: [2, 'Минимальное количество символов - 2'],
-    maxlength: [30, 'Максимальное количество символов - 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   about: {
     type: String,
-    required: [true, 'Поле about не заполнено'],
-    minlength: [2, 'Минимальное количество символов - 2'],
-    maxlength: [30, 'Максимальное количество символов - 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   avatar: {
     type: String,
