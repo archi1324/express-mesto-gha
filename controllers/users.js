6c32b0e6e486406d4e81dcdbaf5c13ef8f9a16f2
Артур Саяхов <artursaahov@MacBook-Air-Artur.local>
2023-09-18 22:13:51 +0500

    fourth try fix id user

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,6 @@ module.exports.createUser = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById({ _id: req.params.userId })
-    .orFail(new Error('Пользователь не найеден'))
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
