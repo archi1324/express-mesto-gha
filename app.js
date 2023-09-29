b610b978ffd0f8291827208bebaa5cb1b1c53b9a
Артур Саяхов <artursaahov@MacBook-Air-Artur.local>
2023-09-29 20:20:24 +0500

    14pr fix errors

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use('/users', auth, require('./routes/users'));
-app.use('/cards', auth, require('./routes/cards'));
+app.use(auth);
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use(errors());
 
